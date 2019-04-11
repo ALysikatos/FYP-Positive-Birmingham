6cf9782171cfb04de3095986f25d995a47cab6bc
Anastasia Lysikatos <30287130+ALysikatos@users.noreply.github.com>
2019-04-11 20:41:20 +0100

    working popup with title, image, rating and website link

--- app/src/main/java/com/example/positivebirmingham/PopUpInfoWindow.java
+++ app/src/main/java/com/example/positivebirmingham/PopUpInfoWindow.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.location.Address;
@@ -16,17 +17,30 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.PhotoMetadata;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.FetchPhotoRequest;
+import com.google.android.libraries.places.api.net.FetchPlaceRequest;
+import com.google.android.libraries.places.api.net.PlacesClient;
+
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
+
+import static com.android.volley.VolleyLog.TAG;
 
 public class PopUpInfoWindow extends Activity {
     LinearLayout linearLayout1;
@@ -59,11 +73,8 @@ public class PopUpInfoWindow extends Activity {
        setContentView(R.layout.popup);
         //getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         setLayout();
-        try {
-            getPlaceDetails();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        place();
+        getPhoto();
     }
 
     private void setLayout(){
@@ -83,8 +94,8 @@ public class PopUpInfoWindow extends Activity {
 //            }
 //        });
 
-        TextView title = findViewById(R.id.txtTitle);
-        title.setText(markerTitle);
+ //       TextView title = findViewById(R.id.txtTitle);
+ //       title.setText(markerTitle);
 
 
     }
@@ -95,14 +106,89 @@ public class PopUpInfoWindow extends Activity {
 
       //  String url ="https://maps.googleapis.com/maps/api/geocode/json?" + latlng +
        //         "&location_type=ROOFTOP&result_type=street_address&key=" + getString(R.string.google_directions_key);
-       // Geocoder geocoder = new Geocoder(this, Locale.getDefault());
-        //List<Address> addresses = geocoder.getFromLocation(markerPosition.latitude, markerPosition.longitude, 1);
-     //   Geocoder geo = new Geocoder();
-       // geocoder = new google.maps.Geocoder();
-      //  Log.i("lol", String.valueOf(addresses));
+    }
 
+    private void place(){
+        // Initialize Places.
+        Places.initialize(getApplicationContext(), getString(R.string.google_directions_key));
+
+// Create a new Places client instance.
+        PlacesClient placesClient = Places.createClient(this);
+
+        // Specify the fields to return.
+        List<Place.Field> placeFields = Arrays.asList(Place.Field.NAME, Place.Field.WEBSITE_URI,
+                Place.Field.RATING, Place.Field.USER_RATINGS_TOTAL);
+
+// Construct a request object, passing the place ID and fields array.
+        FetchPlaceRequest request = FetchPlaceRequest.builder(markerPlaceID, placeFields)
+                .build();
+
+        // Add a listener to handle the response.
+        placesClient.fetchPlace(request).addOnSuccessListener((response) -> {
+            Place place = response.getPlace();
+            Log.i(TAG, "Place found: " + place.getName());
+            if (!place.getName().isEmpty()) {
+                TextView title = findViewById(R.id.txtTitle);
+                title.setText(place.getName());
+            }
+            Log.i(TAG, "Place found: " + place.getWebsiteUri());
+            if (!(place.getWebsiteUri() == null)){
+                TextView url = findViewById(R.id.txtLink);
+                url.setText(place.getWebsiteUri().toString());
+            }
+            if (!(place.getRating() == null)) {
+                int rate = (int) Math.round(place.getRating());
+                RatingBar rateBar = findViewById(R.id.ratingBar);
+                rateBar.setRating(rate);
+                rateBar.setIsIndicator(true);
+            }
+            if (!(place.getUserRatingsTotal() == null)) {
+                TextView noOfRatings = findViewById(R.id.txtRatings);
+                noOfRatings.setText("(Average rating from " + place.getUserRatingsTotal() + " google reviews)");
+            }
+
+        }).addOnFailureListener((exception) -> {
+            if (exception instanceof ApiException) {
+                ApiException apiException = (ApiException) exception;
+                int statusCode = apiException.getStatusCode();
+                // Handle error with given status code.
+                Log.e(TAG, "Place not found: " + exception.getMessage());
+            }
+        });
+    }
+
+    private void getPhoto(){
+        List<Place.Field> fields = Arrays.asList(Place.Field.PHOTO_METADATAS);
+        FetchPlaceRequest placeRequest = FetchPlaceRequest.builder(markerPlaceID, fields).build();
+        PlacesClient placesClient = Places.createClient(this);
+        placesClient.fetchPlace(placeRequest).addOnSuccessListener((response) -> {
+            Place place = response.getPlace();
+
+            // Get the photo metadata.
+            PhotoMetadata photoMetadata = place.getPhotoMetadatas().get(0);
+
+            // Get the attribution text.
+            String attributions = photoMetadata.getAttributions();
+
+            // Create a FetchPhotoRequest.
+            FetchPhotoRequest photoRequest = FetchPhotoRequest.builder(photoMetadata)
+                    .setMaxWidth(700) // Optional.
+                    .setMaxHeight(700) // Optional.
+                    .build();
+            placesClient.fetchPhoto(photoRequest).addOnSuccessListener((fetchPhotoResponse) -> {
+                Bitmap bitmap = fetchPhotoResponse.getBitmap();
+                ImageView image = findViewById(R.id.archImage);
+                image.setImageBitmap(bitmap);
+            }).addOnFailureListener((exception) -> {
+                if (exception instanceof ApiException) {
+                    ApiException apiException = (ApiException) exception;
+                    int statusCode = apiException.getStatusCode();
+                    // Handle error with given status code.
+                    Log.e(TAG, "Place not found: " + exception.getMessage());
+                }
+            });
+        });
 
 
     }
-
 }
