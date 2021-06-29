bfb90039b652bc7082f7c707da7a2861443fabd1
Anastasia Lysikatos <30287130+ALysikatos@users.noreply.github.com>
2021-06-29 16:39:19 +0100

    updated obsolete architecture place IDS'

--- app/src/main/java/com/example/positivebirmingham/PointsParser.java
+++ app/src/main/java/com/example/positivebirmingham/PointsParser.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import static com.example.positivebirmingham.MapsActivity.architectHashmap;
 import static com.example.positivebirmingham.MapsActivity.loadingDialog;
 
 /**
@@ -110,11 +111,13 @@ public class PointsParser extends AsyncTask<String, Integer, List<List<HashMap<S
             miles = distanceInMiles.doubleValue() * 0.62137;
             DecimalFormat df = new DecimalFormat("0.0");
             miles = Double.valueOf(df.format(miles));
-            if (x <= 35) {
+            if (x <= architectHashmap.size()) {
+                Log.d("tag", Integer.toString(x));
+                Log.d("tag", Integer.toString(architectHashmap.size()));
                 //add all architecture distances and durations into arraylists
                 distanceList.add(miles.floatValue());
                 durationList.add(duration);
-                if (x == 35) {
+                if (x == architectHashmap.size()) {
                     loadingDialog.dismiss();
                 }
                 return;
